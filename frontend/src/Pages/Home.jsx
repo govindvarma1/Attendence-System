1286f7212c8faba83932e1150fb030e5642062ae
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2023-10-26 22:43:16 +0530

    tailwind configured

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -17,5 +17,9 @@ export default function Home() {
     Check();
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
